0d56c49b6801074132845c496414af68eaebb290
NAmaral <26823958+N-Amaral@users.noreply.github.com>
2022-11-05 19:09:23 +0000

    Few changes to Update func,still doesn't work

--- todolist/src/App.tsx
+++ todolist/src/App.tsx
@@ -7,7 +7,6 @@ import { state } from "./helper";
 const App = () => {
   //startup list of to-do's
   const storage: string | null = localStorage.getItem("todoList");
-  const editedText: { current: string } = useRef("text");
   const [todoList, setTodoList] = useState(storage === null ? state : JSON.parse(storage));
 
   function updateLocalStorage(todoList: Array<Object>) {
@@ -53,40 +52,45 @@ const App = () => {
   //allows for editing of selected to-do
   function editTodo(id: number) {
     const todos: NodeListOf<HTMLInputElement> = document.querySelectorAll(".todoListItem");
-    const textArea = todos[id].children[1];
-    const dateArea = todos[id].children[2];
-    const btns = todos[id].children[3].children;
+    const textArea: any = todos[id].children[1].children[0];
+    const dateArea = todos[id].children[1].children[1];
+    const btns: NodeListOf<HTMLButtonElement> = todos[id].querySelectorAll("button");
 
-    btns[1].classList.add("hidden");
-    btns[2].classList.remove("hidden");
+    btns[1].parentElement!.classList.add("hidden");
+    btns[2].parentElement!.classList.remove("hidden");
     textArea.removeAttribute("disabled");
+    textArea.value = null;
     dateArea.removeAttribute("disabled");
 
-    textArea.innerHTML = `${todoList[id].textValue}`;
-    dateArea.innerHTML = `${todoList[id].dateValue}`;
+    console.log("edit");
   }
+
+  //logger
+  function handleChange(event: any, id: number) {}
 
   //saves the previously selected for editing to-do
   function saveEditedTodo(id: number) {
     const todos: NodeListOf<HTMLInputElement> = document.querySelectorAll(".todoListItem");
-    const textArea: any = todos[id].children[1];
-    const dateArea = todos[id].children[2];
-    const btns = todos[id].children[3].children;
+    const textArea = todos[id].children[1].children[0];
+    const dateArea = todos[id].children[1].children[1];
+    const btns: NodeListOf<HTMLButtonElement> = todos[id].querySelectorAll("button");
 
-    btns[2].classList.add("hidden");
-    btns[1].classList.remove("hidden");
+    btns[2].parentElement!.classList.add("hidden");
+    btns[1].parentElement!.classList.remove("hidden");
     textArea.setAttribute("disabled", "");
     dateArea.setAttribute("disabled", "");
+
+    console.log(textArea.textContent);
   }
 
   return (
     <div className="min-w-screen max-w-full min-h-screen max-h-full bg-gradient-to-b from-indigo-500 via-purple-500 to-blue-500 ">
       <div className="App w-content h-content p-1 flex">
-        <div className="input-wrapper m-5 p-2 bg-indigo-100 rounded-xl  w-min h-min ">
+        <div className="input-wrapper m-5 p-2 bg-indigo-100 rounded-xl  w-min h-min drop-shadow-lg">
           <InputComponents handleSubmit={newTodo} />
         </div>
         <div className="list-wrapper m-2 p-3 rounded-xl ">
-          <ListComponents content={todoList} handleDelete={deleteTodo} handleEdit={editTodo} handleSave={saveEditedTodo} />
+          <ListComponents content={todoList} handleDelete={deleteTodo} handleEdit={editTodo} handleSave={saveEditedTodo} handleChange={handleChange} />
         </div>
       </div>
     </div>
--- todolist/src/ListComponents.tsx
+++ todolist/src/ListComponents.tsx
@@ -2,53 +2,23 @@
 import React, { useEffect, useMemo } from "react";
 
 //Component for btns in each to-do
-const ListItemBtns = (props: { id: number; delete: Function; edit: Function; save: Function }) => {
+const ListItemBtns = () => {
   return (
     <div className="flex flex-row">
       <span className="pr-1 pl-1 mr-1 ml-1">
-        <form
-          action=""
-          method="GET"
-          onSubmit={(event) => {
-            event.preventDefault();
-            event.stopPropagation();
-            props.delete(props.id);
-          }}
-        >
-          <button className="mt-2 mb-2 mr-1 ml-1 rounded-full bg-slate-100 hover:bg-blue-800 outline outline-white w-20" type="submit">
-            Delete
-          </button>
-        </form>
+        <button className="mt-2 mb-2 mr-1 ml-1 rounded-full bg-slate-100 hover:bg-blue-800 outline outline-white w-20" type="submit" value={"delete"}>
+          Delete
+        </button>
       </span>
       <span className="pr-1 pl-1 mr-1 ml-1">
-        <form
-          action=""
-          method="GET"
-          onSubmit={(event) => {
-            event.preventDefault();
-            event.stopPropagation();
-            props.edit(props.id);
-          }}
-        >
-          <button className="mt-2 mb-2 mr-1 ml-1 rounded-full bg-slate-100  hover:bg-blue-800  outline outline-white w-20" type="submit">
-            Edit
-          </button>
-        </form>
+        <button className="mt-2 mb-2 mr-1 ml-1 rounded-full bg-slate-100  hover:bg-blue-800  outline outline-white w-20" value={"edit"} type="submit">
+          Edit
+        </button>
       </span>
       <span className="pr-1 pl-1 mr-1 ml-1 hidden">
-        <form
-          action=""
-          method="GET"
-          onSubmit={(event) => {
-            event.preventDefault();
-            event.stopPropagation();
-            props.save(props.id);
-          }}
-        >
-          <button className="rounded-full mt-2 mb-2 mr-1 ml-1 bg-slate-100 hover:bg-blue-800 outline outline-white w-20" type="submit">
-            Save
-          </button>
-        </form>
+        <button className="rounded-full mt-2 mb-2 mr-1 ml-1 bg-slate-100 hover:bg-blue-800 outline outline-white w-20" value={"save"} type="submit">
+          Save
+        </button>
       </span>
     </div>
   );
@@ -61,6 +31,7 @@ const ListItem = (props: {
   delete: Function;
   edit: Function;
   save: Function;
+  handleChange: Function;
 }) => {
   const memoValue = useMemo(() => props.content, [props.content]);
   return (
@@ -69,31 +40,61 @@ const ListItem = (props: {
       id={`${props.id}`}
     >
       <span className="font-mono font-bold text-xl pr-1 pl-1 mr-1 ml-1">Created on : {props.content.createdOn}</span>
-      <textarea className="readTodoText font-mono " cols={20} rows={5} disabled placeholder={memoValue.textValue}></textarea>
-      <input
-        type="text"
-        className="readTodoDate w-3/5 font-mono "
-        disabled
-        placeholder={memoValue.dateValue}
-        onFocus={(e) => {
-          e.target.type = "date";
+      <form
+        action=""
+        method="GET"
+        onSubmit={(event: any) => {
+          const submitter = event.nativeEvent.submitter.value;
+          event.preventDefault();
+          event.stopPropagation();
+          switch (submitter) {
+            case "delete":
+              props.delete(props.id);
+              break;
+            case "edit":
+              props.edit(props.id);
+              break;
+            case "save":
+              props.save(props.id);
+              break;
+          }
         }}
-        onBlur={(e) => {
-          e.target.type = "text";
-        }}
-      />
-      <ListItemBtns id={props.id} delete={props.delete} edit={props.edit} save={props.save} />
+      >
+        <textarea
+          className="readTodoText font-mono "
+          cols={20}
+          rows={5}
+          disabled
+          onChange={(event) => props.handleChange(event, props.id)}
+          value={memoValue.textValue}
+        ></textarea>
+        <input
+          type="text"
+          className="readTodoDate w-3/5 font-mono "
+          disabled
+          value={memoValue.dateValue}
+          onFocus={(e) => {
+            e.target.type = "date";
+          }}
+          onBlur={(e) => {
+            e.target.type = "text";
+          }}
+        />
+        <ListItemBtns />
+      </form>
     </li>
   );
 };
 
 //Component with all to-dos
-const List = (props: { content: Array<Object>; handleDelete: Function; handleEdit: Function; handleSave: Function }) => {
+const List = (props: { content: Array<Object>; handleDelete: Function; handleEdit: Function; handleSave: Function; handleChange: Function }) => {
   const memoList = useMemo(() => props.content, [props.content]);
 
   const content: any[] = [];
   memoList.forEach((item: any, i) => {
-    content.push(<ListItem content={item} id={i} key={i} delete={props.handleDelete} edit={props.handleEdit} save={props.handleSave} />);
+    content.push(
+      <ListItem content={item} id={i} key={i} delete={props.handleDelete} edit={props.handleEdit} save={props.handleSave} handleChange={props.handleChange} />
+    );
   });
   return <ul className="m-3 p-2 flex flex-wrap flex-auto flex-row overflow-y-auto h-auto w-auto justify-start">{content}</ul>;
 };
