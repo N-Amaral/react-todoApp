72858ec0cb73c3cf63860e59c0d33c492fd8ede3
NAmaral <26823958+N-Amaral@users.noreply.github.com>
2022-11-08 19:26:53 +0000

    Fixed edit function, added new styles

--- todolist/src/App.tsx
+++ todolist/src/App.tsx
@@ -51,46 +51,49 @@ const App = () => {
 
   //allows for editing of selected to-do
   function editTodo(id: number) {
-    const todos: NodeListOf<HTMLInputElement> = document.querySelectorAll(".todoListItem");
-    const textArea: any = todos[id].children[1].children[0];
-    const dateArea = todos[id].children[1].children[1];
+    const todos = document.querySelectorAll(".todoListItem");
+    const textArea = todos[id].querySelectorAll(".editForm");
+
     const btns: NodeListOf<HTMLButtonElement> = todos[id].querySelectorAll("button");
 
     btns[1].parentElement!.classList.add("hidden");
     btns[2].parentElement!.classList.remove("hidden");
-    textArea.removeAttribute("disabled");
-    textArea.value = null;
-    dateArea.removeAttribute("disabled");
-
-    console.log("edit");
+    textArea[0].classList.remove("hidden");
+    textArea[0].removeAttribute("disabled");
   }
-
-  //logger
-  function handleChange(event: any, id: number) {}
 
   //saves the previously selected for editing to-do
   function saveEditedTodo(id: number) {
     const todos: NodeListOf<HTMLInputElement> = document.querySelectorAll(".todoListItem");
-    const textArea = todos[id].children[1].children[0];
-    const dateArea = todos[id].children[1].children[1];
+    const textArea: any = todos[id].querySelectorAll(".editForm");
     const btns: NodeListOf<HTMLButtonElement> = todos[id].querySelectorAll("button");
 
     btns[2].parentElement!.classList.add("hidden");
     btns[1].parentElement!.classList.remove("hidden");
-    textArea.setAttribute("disabled", "");
-    dateArea.setAttribute("disabled", "");
 
-    console.log(textArea.textContent);
+    textArea[0].classList.add("hidden");
+    textArea[0].setAttribute("disabled", "");
+
+    const newContent = textArea[0].value;
+    setTodoList((prevState: Array<Object>) => {
+      // eslint-disable-next-line array-callback-return
+      const updatedState = prevState.map((item: object, i: number) => {
+        if (id === i) {
+          return { ...item, textValue: newContent };
+        }
+        return item;
+      });
+      updateLocalStorage(updatedState);
+      return updatedState;
+    });
   }
 
   return (
     <div className="min-w-screen max-w-full min-h-screen max-h-full bg-gradient-to-b from-indigo-500 via-purple-500 to-blue-500 ">
-      <div className="App w-content h-content p-1 flex">
-        <div className="input-wrapper m-5 p-2 bg-indigo-100 rounded-xl  w-min h-min drop-shadow-lg">
+      <div className="App w-content h-content p-1 flex justify-center">
+        <div className="input-wrapper m-5 p-2  rounded-xl  w-min h-min drop-shadow-lg">
           <InputComponents handleSubmit={newTodo} />
-        </div>
-        <div className="list-wrapper m-2 p-3 rounded-xl ">
-          <ListComponents content={todoList} handleDelete={deleteTodo} handleEdit={editTodo} handleSave={saveEditedTodo} handleChange={handleChange} />
+          <ListComponents content={todoList} handleDelete={deleteTodo} handleEdit={editTodo} handleSave={saveEditedTodo} />
         </div>
       </div>
     </div>
--- todolist/src/ListComponents.tsx
+++ todolist/src/ListComponents.tsx
@@ -5,19 +5,34 @@ import React, { useEffect, useMemo } from "react";
 const ListItemBtns = () => {
   return (
     <div className="flex flex-row">
-      <span className="pr-1 pl-1 mr-1 ml-1">
-        <button className="mt-2 mb-2 mr-1 ml-1 rounded-full bg-slate-100 hover:bg-blue-800 outline outline-white w-20" type="submit" value={"delete"}>
-          Delete
+      <span className="mr-1 ml-1 h-2/3 w-min">
+        <button className="mt-2 mb-2 p-1 bg-white rounded hover:bg-blue-800  h-min w-min" type="submit" value={"delete"}>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+            <path
+              fillRule="evenodd"
+              d="M8.75 1A2.75 2.75 0 006 3.75v.443c-.795.077-1.584.176-2.365.298a.75.75 0 10.23 1.482l.149-.022.841 10.518A2.75 2.75 0 007.596 19h4.807a2.75 2.75 0 002.742-2.53l.841-10.52.149.023a.75.75 0 00.23-1.482A41.03 41.03 0 0014 4.193V3.75A2.75 2.75 0 0011.25 1h-2.5zM10 4c.84 0 1.673.025 2.5.075V3.75c0-.69-.56-1.25-1.25-1.25h-2.5c-.69 0-1.25.56-1.25 1.25v.325C8.327 4.025 9.16 4 10 4zM8.58 7.72a.75.75 0 00-1.5.06l.3 7.5a.75.75 0 101.5-.06l-.3-7.5zm4.34.06a.75.75 0 10-1.5-.06l-.3 7.5a.75.75 0 101.5.06l.3-7.5z"
+              clipRule="evenodd"
+            />
+          </svg>
         </button>
       </span>
-      <span className="pr-1 pl-1 mr-1 ml-1">
-        <button className="mt-2 mb-2 mr-1 ml-1 rounded-full bg-slate-100  hover:bg-blue-800  outline outline-white w-20" value={"edit"} type="submit">
-          Edit
+      <span className="mr-1 ml-1">
+        <button className="mt-2 mb-2 p-1 bg-white rounded hover:bg-blue-800   h-min w-min" value={"edit"} type="submit">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+            <path d="M5.433 13.917l1.262-3.155A4 4 0 017.58 9.42l6.92-6.918a2.121 2.121 0 013 3l-6.92 6.918c-.383.383-.84.685-1.343.886l-3.154 1.262a.5.5 0 01-.65-.65z" />
+            <path d="M3.5 5.75c0-.69.56-1.25 1.25-1.25H10A.75.75 0 0010 3H4.75A2.75 2.75 0 002 5.75v9.5A2.75 2.75 0 004.75 18h9.5A2.75 2.75 0 0017 15.25V10a.75.75 0 00-1.5 0v5.25c0 .69-.56 1.25-1.25 1.25h-9.5c-.69 0-1.25-.56-1.25-1.25v-9.5z" />
+          </svg>
         </button>
       </span>
-      <span className="pr-1 pl-1 mr-1 ml-1 hidden">
-        <button className="rounded-full mt-2 mb-2 mr-1 ml-1 bg-slate-100 hover:bg-blue-800 outline outline-white w-20" value={"save"} type="submit">
-          Save
+      <span className="mr-1 ml-1 hidden">
+        <button className="mt-2 mb-2 p-1 bg-white rounded hover:bg-blue-800  h-min w-min" value={"save"} type="submit">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+            <path
+              fillRule="evenodd"
+              d="M3 3.5A1.5 1.5 0 014.5 2h6.879a1.5 1.5 0 011.06.44l4.122 4.12A1.5 1.5 0 0117 7.622V16.5a1.5 1.5 0 01-1.5 1.5h-11A1.5 1.5 0 013 16.5v-13zm10.857 5.691a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 00-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+              clipRule="evenodd"
+            />
+          </svg>
         </button>
       </span>
     </div>
@@ -31,12 +46,10 @@ const ListItem = (props: {
   delete: Function;
   edit: Function;
   save: Function;
-  handleChange: Function;
 }) => {
-  const memoValue = useMemo(() => props.content, [props.content]);
   return (
     <li
-      className="todoListItem m-2 p-2 bg-slate-300 rounded drop-shadow-lg outline outline-slate-200 bg-gradient-to-br from-white via-purple-400 to-blue-600 grid "
+      className="todoListItem m-2 p-2 bg-slate-300 rounded drop-shadow-lg  bg-gradient-to-br from-white via-purple-400 to-blue-600 grid w-1/2 h-2/3"
       id={`${props.id}`}
     >
       <span className="font-mono font-bold text-xl pr-1 pl-1 mr-1 ml-1">Created on : {props.content.createdOn}</span>
@@ -60,26 +73,15 @@ const ListItem = (props: {
           }
         }}
       >
-        <textarea
-          className="readTodoText font-mono "
-          cols={20}
-          rows={5}
-          disabled
-          onChange={(event) => props.handleChange(event, props.id)}
-          value={memoValue.textValue}
-        ></textarea>
-        <input
-          type="text"
-          className="readTodoDate w-3/5 font-mono "
-          disabled
-          value={memoValue.dateValue}
-          onFocus={(e) => {
-            e.target.type = "date";
-          }}
-          onBlur={(e) => {
-            e.target.type = "text";
-          }}
-        />
+        <div>
+          <div className="bg-white font-mono text-xl rounded">
+            <span>{props.content.textValue}</span>
+            <input type="text" defaultValue={props.content.textValue} disabled className="hidden editForm" />
+          </div>
+          <div className="bg-white font-mono  text-xl rounded">
+            <span>{props.content.dateValue}</span>
+          </div>
+        </div>
         <ListItemBtns />
       </form>
     </li>
@@ -87,16 +89,14 @@ const ListItem = (props: {
 };
 
 //Component with all to-dos
-const List = (props: { content: Array<Object>; handleDelete: Function; handleEdit: Function; handleSave: Function; handleChange: Function }) => {
+const List = (props: { content: Array<Object>; handleDelete: Function; handleEdit: Function; handleSave: Function }) => {
   const memoList = useMemo(() => props.content, [props.content]);
 
   const content: any[] = [];
   memoList.forEach((item: any, i) => {
-    content.push(
-      <ListItem content={item} id={i} key={i} delete={props.handleDelete} edit={props.handleEdit} save={props.handleSave} handleChange={props.handleChange} />
-    );
+    content.push(<ListItem content={item} id={i} key={i} delete={props.handleDelete} edit={props.handleEdit} save={props.handleSave} />);
   });
-  return <ul className="m-3 p-2 flex flex-wrap flex-auto flex-row overflow-y-auto h-auto w-auto justify-start">{content}</ul>;
+  return <ul className="m-3 p-2 flex flex-wrap flex-auto flex-row overflow-y-auto h-auto w-auto justify-center">{content}</ul>;
 };
 
 export default List;
